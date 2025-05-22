645de36b5c449e9ce279db467fa80f991017f935
laurinchenbienchen <168191199+laurinchenbienchen@users.noreply.github.com>
2025-05-22 10:33:48 +0200

    aufgabe 6, minilab pytorch aufgabe 1

--- exercise6/mini_lab.py
+++ exercise6/mini_lab.py
@@ -59,3 +59,75 @@ if torch.accelerator.is_available():
     print()
     print("Using torch", torch.__version__)
     print(f"Device tensor is stored on: {tensor.device}")
+
+print('the defalt device is:',torch.get_default_device())
+print('the object is a tensor:',torch.is_tensor(tensor))
+print('the input is a complex datatype:',torch.is_complex(tensor))
+print()
+
+# standard numpy-like indexing and slicing
+tensor = torch.ones(4, 4)
+print(tensor)
+print(f"First row: {tensor[0]}")
+print(f"First column: {tensor[:, 0]}")
+print(f"Last column: {tensor[..., -1]}")
+tensor[:, 1] = 0
+print(tensor)
+
+# joining tensors
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+# arithmetic operations
+# This computes the matrix multiplication between two tensors. y1, y2, y3 will have the same value
+# ``tensor.T`` returns the transpose of a tensor
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+print(y1)
+print(y2)
+
+y3 = torch.rand_like(y1)
+torch.matmul(tensor, tensor.T, out=y3)
+print(y3)
+
+# This computes the element-wise product. z1, z2, z3 will have the same value
+z1 = tensor * tensor
+print(z1)
+z2 = tensor.mul(tensor)
+print(z2)
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
+print(z3)
+
+# **Single-element tensors**
+# If you have a one-element tensor, for example by aggregating all
+# values of a tensor into one value, you can convert it to a Python
+# numerical value using ``item()``:
+
+agg = tensor.sum()
+agg_item = agg.item()
+print(agg_item, type(agg_item))
+
+# tensor to numpy array
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+# change in the tensor reflects in numpy array
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
+
+# numpy array to tensor
+n = np.ones(5)
+t = torch.from_numpy(n)
+print(f"n: {n}")
+print(f"t: {t}")
+
+# changes in the numpy array reflect in the tensor
+np.add(n, 1, out=n)
+print(f"t: {t}")
+print(f"n: {n}")
+print("Using torch", torch.__version__)
+print(f"Device tensor is stored on: {tensor.device}")
